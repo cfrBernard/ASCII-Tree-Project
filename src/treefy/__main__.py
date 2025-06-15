8c3afd7d69d27b20760be8a2881ffa96a01f0c99
C. Bernard <cfr.bernard@yahoo.com>
2025-06-15 17:56:15 +0200

    wip(chore) : intial setup 2

---
+++ src/treefy/__main__.py
@@ -0,0 +1,15 @@
+# import sys
+#
+# from treefy.core.treebuilder import run_cli
+# from treefy.gui.app import run_gui
+#
+#
+# def main():
+#     if "--gui" in sys.argv:
+#         run_gui()
+#     else:
+#         run_cli()
+#
+#
+# if __name__ == "__main__":
+#     main()
